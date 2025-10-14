dd828b3dd74ca2b19e2a9609f0dfcd5cb6add7ff
CB <cabdullahi.maxamed@web.de>
2025-10-14 13:42:31 +0200

    Update pkce_generator.dart

--- lib/auth/pkce_generator.dart
+++ lib/auth/pkce_generator.dart
@@ -21,30 +21,3 @@ class PkceGenerator {
     return base64Url.encode(digest.bytes).replaceAll('=', '');
   }
 }
-
-// TODO: Vor Commit entfernen!
-void main() {
-  print('=== PKCE Generator Test ===\n');
-  
-  final pkce = PkceGenerator();
-  
-  // Test Verifier
-  final verifier = pkce.generateCodeVerifier();
-  print('Code Verifier: $verifier');
-  print('Length: ${verifier.length}');
-  print('Has padding: ${verifier.contains('=')}');
-  
-  // Test Challenge
-  final challenge = pkce.generateCodeChallenge(verifier);
-  print('\nCode Challenge: $challenge');
-  print('Length: ${challenge.length}');
-  print('Has padding: ${challenge.contains('=')}');
-  
-  // Test: Gleicher Verifier = Gleicher Challenge
-  final challenge2 = pkce.generateCodeChallenge(verifier);
-  print('\nDeterministisch (gleicher Challenge): ${challenge == challenge2}');
-  
-  // Test: Neuer Verifier = Anderer Verifier
-  final verifier2 = pkce.generateCodeVerifier();
-  print('Verschiedene Verifier: ${verifier != verifier2}');
-}
