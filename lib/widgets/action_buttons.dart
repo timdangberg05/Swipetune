c2923a183b1b8a39e86e01280d82f73d64a0074f
Valentin <vale.cze@gmail.com>
2025-10-15 12:39:59 +0200

    Automatische Weiterleitung zur Spotify-Authentifizierung am entspr. Button

--- lib/widgets/action_buttons.dart
+++ lib/widgets/action_buttons.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../services/auth_services.dart';
 
 // Der bestehende ActionButton
 class ActionButton extends StatefulWidget {
@@ -74,6 +75,21 @@ class SpotifyButton extends StatefulWidget {
 }
 
 class _SpotifyButtonState extends State<SpotifyButton> {
+
+
+  //Keine finale Implementation (Testzweck), wird u.U. noch angepasst
+  //==============================================
+  final auth = AuthServices(
+    clientId: 'deb60e6c420e48b789b7a205a25df95e',  
+    redirectUri: 'http://127.0.0.1:8080/callback',
+    scopes: [
+      'user-read-email',
+      'playlist-modify-public',
+      'playlist-modify-private',
+      'user-top-read',
+    ],
+  );
+  //==============================================
   bool _isPressed = false;
 
   @override
@@ -81,7 +97,11 @@ class _SpotifyButtonState extends State<SpotifyButton> {
     const spotifyColor = Color(0xFF1DB954);
 
     return GestureDetector(
-      onTapDown: (_) => setState(() => _isPressed = true),
+      /* onTapDown: (_) => setState(() => _isPressed = true), */
+      onTapDown: (_){
+        setState(() => _isPressed = true);
+        auth.openAuthorizeUrl();
+      },
       onTapUp: (_) {
         setState(() => _isPressed = false);
         HapticFeedback.lightImpact();
