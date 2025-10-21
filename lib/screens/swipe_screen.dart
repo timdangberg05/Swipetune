e70036cbee8df9de5f39ff3e6997ea90e0403fea
Valentin <vale.cze@gmail.com>
2025-10-21 13:44:05 +0200

    moin

--- lib/screens/swipe_screen.dart
+++ lib/screens/swipe_screen.dart
@@ -14,6 +14,7 @@ class SwipeHomePage extends StatefulWidget {
 }
 
 class _SwipeHomePageState extends State<SwipeHomePage> with TickerProviderStateMixin {
+  
   bool _isPlaying = false;
   Offset _dragOffset = Offset.zero;
 
