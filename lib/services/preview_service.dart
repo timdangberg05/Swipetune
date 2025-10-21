c915a1cd0ac0dceec402081bfe5e9b95e9e72303
Valentin <vale.cze@gmail.com>
2025-10-21 11:26:39 +0200

    Deezer API added

--- lib/services/preview_service.dart
+++ lib/services/preview_service.dart
@@ -12,7 +12,7 @@ class Preview{
 
   void playPreview()async {
     
-    final List<Track> tracks = await songService.getSearchResults('Slipknot');
+    /* final List<Track> tracks = await songService.getSearchResults('Slipknot');
     for(Track track in tracks){
       if(track.previewUrl != null){
         try{
@@ -28,9 +28,9 @@ class Preview{
 
 
 
-    }
-    
-    
+    } */
+    player.setUrl("https://cdnt-preview.dzcdn.net/api/1/1/0/7/1/0/071903d9430ded2ec139793ad3e9c09d.mp3?hdnea=exp=1761032455~acl=/api/1/1/0/7/1/0/071903d9430ded2ec139793ad3e9c09d.mp3*~data=user_id=0,application_id=42~hmac=c2f98f4cbd49d2255e49a7c9088837f170ff242b42634431e6e0eb9e8d3f497e");
+    player.play();
     
   }
 
