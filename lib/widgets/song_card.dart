1a03c080faed0f9066eff4350675ba3bd958ce35
DjKhalories <elias2003ber@gmail.com>
2025-10-22 14:56:06 +0200

    song card bug fix songname slid down after expand

--- lib/widgets/song_card.dart
+++ lib/widgets/song_card.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:swipetune/models/Track.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'player_bar.dart';
 
 class GlassSongCard extends StatefulWidget {
@@ -23,13 +24,13 @@ class GlassSongCard extends StatefulWidget {
 class _GlassSongCardState extends State<GlassSongCard>
     with SingleTickerProviderStateMixin {
   bool _isExpanded = false;
-  Color _textColor = Colors.white;
+  final Color _textColor = Colors.white;
 
   late final AnimationController _controller;
   late final Animation<double> _liftAnim;
   late final Animation<double> _scaleAnim;
   late final Animation<double> _controlsFadeAnim;
-  late final Animation<double> _titleSlideAnim;
+  late Animation<double> _titleSlideAnim;
   late final Animation<double> _downShiftAnim;
   late final Animation<double> _infoFadeAnim;
 
@@ -43,30 +44,25 @@ class _GlassSongCardState extends State<GlassSongCard>
     );
 
     _liftAnim = Tween<double>(begin: 0, end: -80).animate(
-      CurvedAnimation(
-          parent: _controller,
-          curve: const Interval(0.0, 0.35, curve: Curves.easeOutCubic)),
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic),
     );
+
     _scaleAnim = Tween<double>(begin: 1.0, end: 1.12).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeInOutBack),
     );
+
     _controlsFadeAnim = Tween<double>(begin: 1.0, end: 0.0).animate(
-      CurvedAnimation(
-          parent: _controller,
-          curve: const Interval(0.25, 0.6, curve: Curves.easeOut)),
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
     );
-    _titleSlideAnim = Tween<double>(begin: 0, end: -433).animate(
-      CurvedAnimation(
-          parent: _controller,
-          curve: const Interval(0.25, 0.6, curve: Curves.easeOut)),
-    );
+
+    _titleSlideAnim = const AlwaysStoppedAnimation(0);
+
     _downShiftAnim = Tween<double>(begin: 0, end: 15).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
     );
+
     _infoFadeAnim = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-          parent: _controller,
-          curve: const Interval(0.6, 1.0, curve: Curves.easeIn)),
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
     );
   }
 
@@ -89,140 +85,162 @@ class _GlassSongCardState extends State<GlassSongCard>
     final collapsedHeight = collapsedWidth * 1.65;
     final topMargin = _isExpanded ? 35.0 : 20.0;
 
-    return GestureDetector(
-      onTap: _toggleExpand,
-      child: AnimatedBuilder(
-        animation: _controller,
-        builder: (context, child) {
-          final lift = _liftAnim.value;
-          final scale = _scaleAnim.value;
-          final controlsFade = _controlsFadeAnim.value;
-          final titleSlide = _titleSlideAnim.value;
-          final downShift = _downShiftAnim.value;
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final dynamicSlide = -constraints.maxHeight * 0.46;
 
-          return Transform.translate(
-            offset: Offset(0, lift + downShift),
-            child: Transform.scale(
-              scale: scale,
-              alignment: Alignment.topCenter,
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 400),
-                curve: Curves.easeInOut,
-                width: collapsedWidth * (_isExpanded ? 1.05 : 1.0),
-                height: collapsedHeight * (_isExpanded ? 1.08 : 1.0),
-                margin: EdgeInsets.fromLTRB(
-                    horizontalPadding, topMargin, horizontalPadding, 0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(32),
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-                        child: Container(color: Colors.transparent),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(32),
-                          border: Border.all(
-                              color: Colors.white.withOpacity(0.3), width: 2.5),
-                        ),
-                      ),
-                      ClipRRect(
+        // Animation des Songtitels synchron zur Card
+        _titleSlideAnim = Tween<double>(begin: 0, end: dynamicSlide).animate(
+          CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+        );
+
+        return GestureDetector(
+          onTap: _toggleExpand,
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              final lift = _liftAnim.value;
+              final scale = _scaleAnim.value;
+              final controlsFade = _controlsFadeAnim.value;
+              final downShift = _downShiftAnim.value;
+              double titleSlide =
+                  _titleSlideAnim.value.clamp(dynamicSlide, 0);
+
+              return Transform.translate(
+                offset: Offset(0, lift + downShift),
+                child: Transform.scale(
+                  scale: scale,
+                  alignment: Alignment.topCenter,
+                  child: RepaintBoundary(
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 400),
+                      curve: Curves.easeInOut,
+                      width: collapsedWidth * (_isExpanded ? 1.05 : 1.0),
+                      height: collapsedHeight * (_isExpanded ? 1.08 : 1.0),
+                      margin: EdgeInsets.fromLTRB(
+                          horizontalPadding, topMargin, horizontalPadding, 0),
+                      child: ClipRRect(
                         borderRadius: BorderRadius.circular(32),
-                        child: Image.network(
-                          widget.track.albumImageUrl,
-                          fit: BoxFit.cover,
-                          errorBuilder: (_, __, ___) => _coverFallback(),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(32),
-                          border: Border.all(
-                              color: Colors.white.withOpacity(0.4), width: 3),
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 20,
-                        left: 20,
-                        right: 20,
-                        child: LiquidMusicPlayer(
-                          track: widget.track,
-                          isPlaying: widget.isPlaying,
-                          onPlayPause: widget.onPlayPause,
-                          titleOffset: titleSlide,
-                          controlsFade: controlsFade,
-                          textColor: _textColor,
-                          textShadows: [
-                            Shadow(
-                              offset: const Offset(1.5, 1.5),
-                              blurRadius: 2,
-                              color: Colors.black87,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            // Hintergrund Blur (leicht reduziert)
+                            BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+                              child: Container(color: Colors.transparent),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(32),
+                                border: Border.all(
+                                  color: Colors.white.withOpacity(0.3),
+                                  width: 2.5,
+                                ),
+                              ),
+                            ),
+                            // Gecachtes Albumcover
+                            CachedNetworkImage(
+                              imageUrl: widget.track.albumImageUrl,
+                              fit: BoxFit.cover,
+                              placeholder: (_, __) => _coverFallback(),
+                              errorWidget: (_, __, ___) => _coverFallback(),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(32),
+                                border: Border.all(
+                                  color: Colors.white.withOpacity(0.4),
+                                  width: 3,
+                                ),
+                              ),
+                            ),
+                            // Player Bar
+                            Positioned(
+                              bottom: 20,
+                              left: 20,
+                              right: 20,
+                              child: LiquidMusicPlayer(
+                                track: widget.track,
+                                isPlaying: widget.isPlaying,
+                                onPlayPause: widget.onPlayPause,
+                                titleOffset: titleSlide,
+                                controlsFade: controlsFade,
+                                textColor: _textColor,
+                                textShadows: [
+                                  Shadow(
+                                    offset: const Offset(1.5, 1.5),
+                                    blurRadius: 2,
+                                    color: Colors.black87,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // Track Details
+                            Positioned(
+                              bottom: 50,
+                              left: 25,
+                              right: 25,
+                              child: IgnorePointer(
+                                ignoring: !_isExpanded,
+                                child: Opacity(
+                                  opacity: _infoFadeAnim.value,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      _buildInfoRow(Icons.album, "Album",
+                                          widget.track.albumName),
+                                      const SizedBox(height: 8),
+                                      _buildInfoRow(Icons.calendar_today,
+                                          "Release", widget.track.releaseDate),
+                                      const SizedBox(height: 8),
+                                      _buildInfoRow(Icons.trending_up,
+                                          "Popularity",
+                                          "${widget.track.popularity}%"),
+                                    ],
+                                  ),
+                                ),
+                              ),
                             ),
                           ],
                         ),
                       ),
-                      Positioned(
-                        bottom: 50,
-                        left: 25,
-                        right: 25,
-                        child: IgnorePointer(
-                          ignoring: !_isExpanded,
-                          child: Opacity(
-                            opacity: _infoFadeAnim.value,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                _buildInfoRow(Icons.album, "Album",
-                                    widget.track.albumName),
-                                const SizedBox(height: 8),
-                                _buildInfoRow(Icons.calendar_today, "Release",
-                                    widget.track.releaseDate),
-                                const SizedBox(height: 8),
-                                _buildInfoRow(Icons.trending_up, "Popularity",
-                                    "${widget.track.popularity}/100"),
-                                const SizedBox(height: 8),
-                                _buildInfoRow(Icons.audiotrack, "Preview",
-                                    widget.track.previewUrl != null
-                                        ? "Available"
-                                        : "Not available"),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-          );
-        },
-      ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 
   Widget _coverFallback() => Container(
         color: Colors.grey.shade900.withOpacity(0.5),
         child: const Center(
-          child:
-              Icon(Icons.music_note_rounded, size: 60, color: Colors.white38),
+          child: Icon(Icons.music_note_rounded,
+              size: 60, color: Colors.white38),
         ),
       );
 
-  // InfoRow mit dezenter schwarzer Umrandung
   Widget _buildInfoRow(IconData icon, String label, String value) {
-    const shadow = Shadow(
-      offset: Offset(1.5, 1.5),
-      blurRadius: 2.0,
-      color: Colors.black87,
-    );
+    const shadowOffset = Offset(1.5, 1.5);
+    const shadowColor = Colors.black87;
 
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Icon(icon, color: _textColor, size: 18),
+        Stack(
+          children: [
+            Positioned(
+              left: shadowOffset.dx,
+              top: shadowOffset.dy,
+              child: Icon(icon, color: shadowColor, size: 18),
+            ),
+            Icon(icon, color: _textColor, size: 18),
+          ],
+        ),
         const SizedBox(width: 8),
         Text(
           "$label:",
@@ -230,7 +248,13 @@ class _GlassSongCardState extends State<GlassSongCard>
             color: _textColor,
             fontWeight: FontWeight.w800,
             fontSize: 16,
-            shadows: [shadow],
+            shadows: const [
+              Shadow(
+                offset: shadowOffset,
+                blurRadius: 2,
+                color: shadowColor,
+              ),
+            ],
           ),
         ),
         const SizedBox(width: 6),
@@ -241,7 +265,13 @@ class _GlassSongCardState extends State<GlassSongCard>
               color: _textColor.withOpacity(0.85),
               fontWeight: FontWeight.w600,
               fontSize: 16,
-              shadows: [shadow],
+              shadows: const [
+                Shadow(
+                  offset: shadowOffset,
+                  blurRadius: 2,
+                  color: shadowColor,
+                ),
+              ],
             ),
             overflow: TextOverflow.ellipsis,
           ),
